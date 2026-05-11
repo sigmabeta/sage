cb41d610466c35ff12087f022bf2f806b2fb0739
sigmabeta <bastos.eder@gmail.com>
2026-05-10 21:08:16 -0400

    New component

---
+++ common/ui/components/src/main/java/net/sigmabeta/sage/components/NameCaptionValueListModel.kt
@@ -0,0 +1,13 @@
+package net.sigmabeta.sage.components
+
+import net.sigmabeta.sage.appcomm.SageAction
+
+data class NameCaptionValueListModel(
+    override val dataId: Long,
+    val name: String,
+    val caption: String,
+    val value: String,
+    val clickAction: SageAction,
+) : ListModel() {
+    override val columns = ListModel.COLUMNS_ALL
+}
